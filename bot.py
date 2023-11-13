139430ec357db3104344c3683f25f562676921e5
DBin_K <DBinKv1@Gmail.com>
2023-11-13 09:53:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,8 @@ def query_text(inline_query):
     try:
         clean_url = purify.process_url(text)
         replaced_text = purify.replace_url(text, clean_url)
-        r = types.InlineQueryResultArticle('1', '只有净化后的链接', types.InputTextMessageContent(clean_url))
-        r2 = types.InlineQueryResultArticle('2', '原始文字和净化后的链接', types.InputTextMessageContent(replaced_text))
+        r = types.InlineQueryResultArticle('1', '发送净化后的链接', types.InputTextMessageContent(clean_url))
+        r2 = types.InlineQueryResultArticle('2', '替换原文链接为净化链接发送', types.InputTextMessageContent(replaced_text))
         bot.answer_inline_query(inline_query.id, [r, r2])
     except Exception as e:
         print(e)
